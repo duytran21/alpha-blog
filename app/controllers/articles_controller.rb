278f10720ecb08b8ba599da23fc398910b25420b
duytran21 <alanduy21@yahoo.com>
2017-04-03 01:39:07 -0700

    add styling for new article and flash messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,8 @@ class ArticlesController < ApplicationController
 
     if @article.save
       #do something
-      flash[:notice] = "Article is successfully created"
+      #change flash[:notice] to flash[:success] for _messages new layout.
+      flash[:success] = "Article is successfully created."
       redirect_to article_path(@article)
     else
       render "new"
@@ -40,25 +41,30 @@ class ArticlesController < ApplicationController
   # PATCH/PUT /articles/1
   # PATCH/PUT /articles/1.json
   def update
-    respond_to do |format|
+    #respond_to do |format|
       if @article.update(article_params)
-        format.html { redirect_to @article, notice: 'Article was successfully updated.' }
-        format.json { render :show, status: :ok, location: @article }
+      #  format.html { redirect_to @article, notice: 'Article was successfully updated.' }
+      #  format.json { render :show, status: :ok, location: @article }
+        flash[:success] = "Article was successfully updated."
+        redirect_to article_path(@article)
       else
-        format.html { render :edit }
-        format.json { render json: @article.errors, status: :unprocessable_entity }
+      #  format.html { render :edit }
+      #  format.json { render json: @article.errors, status: :unprocessable_entity }
+        render 'edit'
       end
-    end
+    #end
   end
 
   # DELETE /articles/1
   # DELETE /articles/1.json
   def destroy
     @article.destroy
-    respond_to do |format|
-      format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    #respond_to do |format|
+    #  format.html { redirect_to articles_url, notice: 'Article was successfully destroyed.' }
+    #  format.json { head :no_content }
+    #end
+    flash[:danger] = "Article was successfully destroyed."
+    redirect_to articles_path
   end
 
   private
